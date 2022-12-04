b76ce23ef0f6f40843ef6ddf4eb63c330f132b92
min2oyo <min2u@outlook.com>
2022-12-04 22:44:55 +0900

    3장:롬복 | 6.@EqualsAndHashCode

---
+++ DevProject/src/main/java/org/hdcd/domain/Student.java
@@ -0,0 +1,17 @@
+package org.hdcd.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "studentNo")
+public class Student {
+
+	private int studentNo;
+	private String name;
+
+}
--- DevProject/src/test/java/org/hdcd/LombokTests.java
+++ DevProject/src/test/java/org/hdcd/LombokTests.java
@@ -1,7 +1,13 @@
 package org.hdcd;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.hdcd.domain.Board;
 import org.hdcd.domain.Member;
+import org.hdcd.domain.Student;
+import org.hdcd.domain.UserItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +19,7 @@ public class LombokTests {
 
 		Board board = new Board();
 
+		System.out.println();
 		System.out.println("testNoArgsConstructor" + board);
 
 	}
@@ -22,6 +29,7 @@ public class LombokTests {
 
 		Board board = new Board("테스트 제목");
 
+		System.out.println();
 		System.out.println("testRequiredArgsConstructor: " + board);
 
 	}
@@ -31,6 +39,7 @@ public class LombokTests {
 
 		Board board = new Board();
 
+		System.out.println();
 		System.out.println("testGetter: " + board.getTitle());
 
 	}
@@ -41,6 +50,7 @@ public class LombokTests {
 		Board board = new Board();
 		board.setTitle("게시판제목");
 
+		System.out.println();
 		System.out.println("testSetter: " + board.getTitle());
 
 	}
@@ -50,6 +60,7 @@ public class LombokTests {
 
 		Board board = new Board();
 
+		System.out.println();
 		System.out.println("testToString: " + board);
 
 	}
@@ -59,7 +70,111 @@ public class LombokTests {
 
 		Member member = new Member();
 
+		System.out.println();
 		System.out.println("testToStringExclude: " + member);
+
+	}
+
+	@Test
+	public void testBoard() {
+
+		Board board1 = new Board();
+		board1.setBoardNo(1);
+		board1.setTitle("title1");
+
+		Board board2 = new Board();
+		board1.setBoardNo(2);
+		board1.setTitle("title2");
+
+		Board board3 = new Board();
+		board1.setBoardNo(2);
+		board1.setTitle("title3");
+
+		Set<Board> boardSet = new HashSet<Board>();
+
+		boardSet.add(board1);
+		boardSet.add(board2);
+		boardSet.add(board3);
+
+		System.out.println();
+		System.out.println("testBoard:");
+		System.out.println("저장된 데이터 수: " + boardSet.size());
+
+		Iterator<Board> it = boardSet.iterator();
+
+		while (it.hasNext()) {
+
+			System.out.println(it.next());
+
+		}
+
+	}
+
+	@Test
+	public void testUserItem() {
+
+		UserItem userItem1 = new UserItem();
+		userItem1.setUserNo(1);
+		userItem1.setItemId(100);
+		userItem1.setDescription("userItem1");
+
+		UserItem userItem2 = new UserItem();
+		userItem2.setUserNo(1);
+		userItem2.setItemId(200);
+		userItem2.setDescription("userItem2");
+
+		UserItem userItem3 = new UserItem();
+		userItem3.setUserNo(1);
+		userItem3.setItemId(200);
+		userItem3.setDescription("userItem3");
+
+		Set<UserItem> userItemSet = new HashSet<UserItem>();
+
+		userItemSet.add(userItem1);
+		userItemSet.add(userItem2);
+		userItemSet.add(userItem3);
+
+		System.out.println();
+		System.out.println("testUserItem:");
+		System.out.println("저장된 데이터 수: " + userItemSet.size());
+
+		Iterator<UserItem> it = userItemSet.iterator();
+
+		while (it.hasNext()) {
+
+			System.out.println(it.next());
+
+		}
+
+	}
+
+	@Test
+	public void testStudent() {
+
+		Student student1 = new Student();
+		student1.setStudentNo(1);
+		student1.setName("Alex");
+
+		Student student2 = new Student();
+		student2.setStudentNo(2);
+		student2.setName("Alex");
+
+		Set<Student> studentSet = new HashSet<Student>();
+
+		studentSet.add(student1);
+		studentSet.add(student2);
+
+		System.out.println();
+		System.out.println("testStudent: ");
+		System.out.println("저장된 데이터 수: " + studentSet.size());
+
+		Iterator<Student> it = studentSet.iterator();
+
+		while (it.hasNext()) {
+
+			System.out.println(it.next());
+
+		}
 
 	}
 
