1ee014ea2fab424fd6ecaeedf174178d2d6bd465
min2oyo <min2u@outlook.com>
2022-12-04 20:54:23 +0900

    3장:롬복 | 4.@Getter, @Setter

--- DevProject/src/test/java/org/hdcd/LombokTests.java
+++ DevProject/src/test/java/org/hdcd/LombokTests.java
@@ -25,4 +25,23 @@ public class LombokTests {
 
 	}
 
+	@Test
+	public void testGetter() {
+
+		Board board = new Board();
+
+		System.out.println(board.getTitle());
+
+	}
+
+	@Test
+	public void testSetter() {
+
+		Board board = new Board();
+		board.setTitle("게시판제목");
+
+		System.out.println(board.getTitle());
+
+	}
+
 }
