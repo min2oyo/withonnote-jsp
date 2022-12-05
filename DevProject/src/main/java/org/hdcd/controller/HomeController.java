86bc35258d260670bc02af65f798657fbe9ce685
min2oyo <min2u@outlook.com>
2022-12-05 19:54:57 +0900

    3장:롬복 | 12.@Slf4j

--- DevProject/src/main/java/org/hdcd/controller/HomeController.java
+++ DevProject/src/main/java/org/hdcd/controller/HomeController.java
@@ -1,13 +1,30 @@
 package org.hdcd.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/")
-	public String home() {
+	public String home(Locale locale, Model model) {
+
+		log.info("Welcome home! The client locale is " + locale + ".");
+
+		LocalDateTime now = LocalDateTime.now();
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
+		String formattedNow = now.format(formatter);
+
+		model.addAttribute("serverTime", formattedNow);
 
 		return "home";
 
