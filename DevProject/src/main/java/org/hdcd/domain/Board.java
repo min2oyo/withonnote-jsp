a8d66b583beeabdd1f911ca8954bf677abf0bfbe
min2oyo <min2u@outlook.com>
2022-12-05 19:13:18 +0900

    3장:롬복 | 10.@Data

--- DevProject/src/main/java/org/hdcd/domain/Board.java
+++ DevProject/src/main/java/org/hdcd/domain/Board.java
@@ -3,21 +3,17 @@ package org.hdcd.domain;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@NoArgsConstructor
-@RequiredArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @EqualsAndHashCode(of = "boardNo")
-@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Board {
 
 	private int boardNo;
